d6a05bf53988b71da6d8a30dacd636ca4bd6768c
nam.dao <hoangnam1121@gmail.com>
2020-07-31 13:54:00 +0700

    import authen firebase

--- src/otp/logicFirebase.js
+++ src/otp/logicFirebase.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import auth from '@react-native-firebase/auth';
 import AuthenConfig from '../AuthenConfig';
 
 export const withOTPFirebaseBusinessLogic = WrappedComponent => {
